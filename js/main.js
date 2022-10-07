137a9a97d95cfd112131da77755375b307a4f6ee
Lucas Conti <lucasger78@hotmail.com>
2022-10-07 12:49:37 -0300

    Web PreFinal

--- js/main.js
+++ js/main.js
@@ -29,26 +29,26 @@
 
 
     // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
+    // $(document).ready(function () {
+    //     var $videoSrc;
+    //     $('.btn-play').click(function () {
+    //         $videoSrc = $(this).data("src");
+    //     });
+    //     console.log($videoSrc);
 
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "showinfo=0");
+    //     $('#videoModal').on('shown.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc + "showinfo=0");
             
-        })
+    //     })
 
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
+    //     $('#videoModal').on('hide.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc);
+    //     })
 
-        $('#close-modal').click(() => {
-            $("#video").removeAttr('src');
-            });
-    });
+    //     $('#close-modal').click(() => {
+    //         $("#video").removeAttr('src');
+    //         });
+    // });
     
     
     // Back to top button
